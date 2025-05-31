9168d4f00ddf4b53bc3cf904ea055bf2c5ddc956
VicthorDuarte <vduarteffontoura@gmail.com>
2025-05-31 10:55:42 -0300

    Add files via upload

--- aula06.py
+++ aula06.py
@@ -37,7 +37,7 @@ class pessoa:
     def __init__ (self,nome):
         self.nome = nome
     def __str__(self):
-        return f'Pessoa: {self.nome}'
+        return f'Meu nome é: {self.nome}'
     def __repr__(self):
         return f'pessoa(nome={self.nome!r})'
 class produto:
@@ -46,9 +46,31 @@ class produto:
     def __eq__(self, other):
         return self.preco == other.preco
 class Minhalista:
-    def __init_ (self, itens):
+    def __init__ (self, itens):
         self.itens = itens
     def __len__(self):
         return len(self.itens)
     def __getitem__(self, index):
         return self.itens[index]
+p1 = produto (10.0)
+p2 = produto (10.0)
+print(p1==p2)
+lista1 = ['Chinelo', 'Camisa', 'Calça','Casaco', 'Bode', 'Bermuda','Boné','Boina']
+minha_lista = Minhalista(lista1)
+print(f'O comprimento da lista é de: {len(minha_lista)}')  # Saída: 8
+print(minha_lista[0])  # Saída: chnelo
+
+class Numero:
+    def __init__(self, valor):
+        self.valor = valor
+    
+    def __add__(self, outro):
+        return Numero(self.valor + outro.valor)
+    
+    def __str__(self):
+        return (f'{self.valor}')
+    
+p1 = Numero(99)
+p2 = Numero(1)
+
+print (p1+p2)
